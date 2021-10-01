bc3a89b14ec25e0be33f2537617d561319693dc9
bonettimrc <bonetti.mrc@gmail.com>
2021-10-01 11:16:59 +0200

    removed useless requires

--- exercises/2/02.script_esterni.php
+++ exercises/2/02.script_esterni.php
@@ -25,54 +25,8 @@
 <?php
 	
 	// Include uno script esterno anche più volte
-	require("02-esterno.php");
-	require("02-esterno.php");
-	require("02-esterno.php");
-
-	// Include uno script con definizione di funzioni
-	// Se incluso più volte genera un errore 
-	// in quanto la/le funzioni sono definite più volte
-	require("02-esternoF.php");
-	//require("02-esternoF.php");
-	
-	// Questa istruzione genera un errore irreversibile
-	// impedendo il proseguimento dello script
-	//require("03-esterno.php"); 
-	
-	// Include uno script esterno UNA SOLA VOLTA
-	// evita principalmente errori di ridefinizione
-	// provare ad utilizzare le due istruzioni individualmente
-	require("02-esternoF.php");
-	require_once("02-esternoF.php");
-	
-	echo "<hr>";
-	echo "<p>Tutto OK: frutto:$frutto | variet&agrave;:$varieta<p>";
-	stampa();
-
-//--------------------------------------------------------------------------------
-	
-	// Include uno script esterno anche più volte
-	include("02-esterno.php");
-	include("02-esterno.php");
-	include("02-esterno.php");
-
-
-	// Include uno script con definizione di funzioni
-	// Se incluso più volte genera un errore 
-	// in quanto la/le funzioni sono definite più volte
-	include("02-esternoF.php");
-	//include("02-esternoF.php");
-	
-	// Questa istruzione genera un errore 
-	// lo script prosegue ugualmente
-	//include("03-esterno.php"); 
-
-	// Include uno script esterno UNA SOLA VOLTA
-	// evita principalmente errori di ridefinizione
-	// provare ad utilizzare le due istruzioni individualmente
-	include("02-esternoF.php");
-	include_once("02-esternoF.php");
-	
+	require("02.esterno.php");
+	require("02.esternoF.php");
 	echo "<hr>";
 	echo "<p>Tutto OK: frutto:$frutto | variet&agrave;:$varieta<p>";
 	stampa();
