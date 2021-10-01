54e087b7af22b0d1d793980fd203767616d4cf7f
bonettimrc <bonetti.mrc@gmail.com>
2021-10-01 12:02:40 +0200

    exercise 3

---
+++ exercises/3/invia.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>03.invia</title>
+</head>
+<body>
+    <form method="GET" action="03.ricevi.php">
+		<label for="txtNome">Nome</label>
+		<br>
+		<input type="text" name="txtNome">
+		<br>
+		<input type="submit" value="Invia">
+	</form>
+</body>
+</html>
